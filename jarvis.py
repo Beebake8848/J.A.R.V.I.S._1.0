ce6145642e5f048c0898725d3f38915f67c1346c
Beebake8848 <kbibek448@gmail.com>
2023-09-18 19:53:55 +0545

    added

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,11 @@
+import sys
 import pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia 
 import webbrowser
 import os
+# import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -45,19 +47,33 @@ def takeCommand():
         return "None"
     return query
     
+# def sendEmail(to, content):
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.ehlo()
+#     server.starttls()
+#     server.login('youremail@gmail.com', 'your-password')
+#     server.sendmail('youremail@gmail.com', to, content)
+#     server.close()
 
 if __name__ == "__main__":
     wishMe()
     while True:
+    # if 1:
         query = takeCommand().lower()
 
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
-            query = query.replace("wikipeda", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
+            query = query.replace("wikipedia", "")
+            try:
+                results = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+            except wikipedia.exceptions.PageError:
+                speak(f"Sorry, I couldn't find any information on '{query}' on Wikipedia.")
+            except Exception as e:
+                speak(f"An error occurred: {e}")
+
 
         elif 'open youtube' in query:
             webbrowser.open("youtube.com")
@@ -65,5 +81,32 @@ if __name__ == "__main__":
         elif 'open google' in query:
             webbrowser.open("google.com")
 
-        elif 'open chatgpt' in query:
-            webbrowser.open("chat.openai.com")
+        elif 'open chat gpt' in query:
+            speak("Opening the OpenAI GPT-3 chat interface.")
+            webbrowser.open("https://beta.openai.com/")
+
+        # elif 'play music' in query:
+            # pass music directory
+        
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, The time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Dell\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'stop jarvis' in query or 'exit jarvis' in query:
+            speak("Thank you for using Jarvis! Have a great day! Goodbye!")
+            sys.exit()
+
+        # elif 'email to bibek' in query:
+        #     try:
+        #         speak("What should I say?")
+        #         content = takeCommand()
+        #         to = "bibekyourEmail@gmail.com"    
+        #         sendEmail(to, content)
+        #         speak("Email has been sent!")
+        #     except Exception as e:
+        #         print(e)
+        #         speak("Sorry Bibek Sir. I am not able to send this email")
